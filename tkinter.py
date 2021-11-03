afc593b441fc0b031108c0df8f3c62e6eda666d4
apurv0701 <apurv07012001@gmail.com>
2021-11-03 10:58:10 +0530

    Create tkinter.py
    First file trying to commit through github desktop

---
+++ tkinter.py
@@ -0,0 +1,21 @@
+from sys import exit as e
+from tkinter import *
+def main ():
+    root = Tk()
+    button = Button(master=root,activebackground = "blue",
+                    text='hello',
+                    command = quit_callback)
+    button.pack()
+    
+    #root = Tk()
+    button = Button(root,text='h',command = quit_callback)
+    button.pack()
+    root.mainloop()
+   
+def quit_callback():
+    #sys.exit(0)
+    e(0)
+    
+
+
+main()
