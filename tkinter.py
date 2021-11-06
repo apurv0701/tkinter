543320c1244114b1ea800c125d950fe9c3bfbe80
apurv0701 <apurv07012001@gmail.com>
2021-11-06 22:44:38 +0530

    Update tkinter.py
    a simple GUI for calculator created using button method

--- tkinter.py
+++ tkinter.py
@@ -1,99 +1,287 @@
-from sys import exit as e
+#fill argument in pack method− Determines whether widget fills any extra space allocated to it by the packer,
+
+#or keeps its own minimal dimensions: NONE (default), X (fill only horizontally),
+
+#Y (fill only vertically), or BOTH (fill both horizontally and vertically).
+
+ 
+
+ 
+
+   
+
 from tkinter import *
+
+ 
+
+num1=0
+
+num2=0
+
+ 
+
 root = Tk()
-def main ():
-    #root = Tk()
-    button = Button(master=root,activebackground = "blue",
-                    text='hello',
-                    command = quit_callback)
-    button.pack()
+
+ 
+
+ 
+
+def pressed_button_1():
+
+    global num1
+
+    num1=1
+
+    print("one")
+
+ 
+
+def pressed_button_2():
+
+    global num1
+
+    num1=2
+
+    print("two")
+
+ 
+
+def pressed_button_3():
+
+    global num1
+
+    num1=3
+
+    print("three") 
+
     
-    #root = Tk()
-    button = Button(root,text='h',command = quit_callback)
-    button.pack()
-    root.mainloop()
-   
-def quit_callback():
-    #sys.exit(0)
-    e(0)
-    
-#no change, only to see difference on GitHub
 
-    #add design
-def design():
-   # root = Tk()
-    w = Canvas(root, width=40, height=60)
-    w.grid()
-    canvas_height=20
-    canvas_width=200
-    y = int(canvas_height / 2)
-    w.create_line(0, y, canvas_width, y )
+def pressed_button_4():
 
-    #add checkbox
-def checkbox():
-    #root = Tk()
-    root.title('choose gender')
+    global num1
 
-    var1 = IntVar()
-    Checkbutton(root, text='male', variable=var1).grid(row=0, sticky=W)
-    var2 = IntVar()
-    Checkbutton(root, text='female', variable=var2).grid(row=1, sticky=W)
-   # Checkbutton.pack()
-   
-    #add label
-def label() :
-    #root = Tk()
-    Label(root, text='First Name').grid(row=0)
-    Label(root, text='Last Name').grid(row=1)
-    e1 = Entry(root)
-    e2 = Entry(root)
-    e1.grid(row=0, column=1)
-    e2.grid(row=1, column=1)
+    num1 =4
 
+    print("four")
 
-    #frame
-def frame():
-    frame = Frame(root)
-    frame.pack()
-    bottomframe = Frame(root)
-    bottomframe.pack( side = TOP )
-    redbutton = Button(frame, text = 'Red', fg ='red')
-    redbutton.pack( side = RIGHT)
-    greenbutton = Button(frame, text = 'Brown', fg='brown')
-    greenbutton.pack( side = LEFT )
-    bluebutton = Button(frame, text ='Blue', fg ='blue')
-    bluebutton.pack( side = LEFT )
-    blackbutton = Button(bottomframe, text ='Black', fg ='black')
-    blackbutton.pack( side = BOTTOM)
-    #root.mainloop()    
+ 
 
-    #LABEL
-def label():
-    w = Label(root, text='GeeksForGeeks.org!')
-    w.pack()
-    root.mainloop()
+def pressed_button_5():
 
-    #listbox
-def listbox():
-   
-    Lb = Listbox(root)
-    Lb.insert(1, 'Python')
-    Lb.insert(2, 'Java')
-    Lb.insert(3, 'C++')
-    Lb.insert(4, 'Any other')
-    Lb.pack()
-    root.mainloop()
+    global num1
+
+    num1=5
+
+    print("five")
+
+ 
+
+def pressed_button_6():
+
+    global num1
+
+    num1=6
+
+    print("six")
+
+ 
+
+def pressed_button_7():
+
+    global num1
+
+    num1=7
+
+    print("seven")
+
+ 
+
+def pressed_button_8():
+
+    global num1
+
+    num1=8
+
+    print("eight")
+
+ 
+
+def pressed_button_9():
+
+    global num1
+
+    num1=9
+
+    print("nine")
+
+ 
+
+def pressed_button_0():
+
+    global num1
+
+    num1=0
+
+    print("zero")
+
    
 
+ 
 
-#main()
-#design()
-#checkbox()
-#label()
-#frame()
-#label()
-listbox()
+def pressed_button_add():
 
+    ans = num1 + num2
 
+    print("add")
 
+    print(ans)
 
+   
+
+ 
+
+def pressed_button_sub():
+
+    ans = num1 - num2
+
+    print("subtraction")
+
+    print(ans)
+
+ 
+
+def pressed_button_mul():
+
+    ans = num1 * num2
+
+    print("mutiplication")
+
+    print(ans)
+
+ 
+
+def pressed_button_div():
+
+    ans = num1 / num2
+
+    print("division")
+
+    print(ans)
+
+ 
+
+def main():
+
+   root.title("STANDARD CALCULATOR")
+
+   root.geometry('300x300')
+
+ 
+
+   frame = Frame(root)
+
+   frame.pack()
+
+ 
+
+  # equation2 = IntVar()
+
+   #expression_field2 = Entry(root, textvariable=equation2)
+
+   #expression_field2.grid(columnspan=4, ipadx=70)
+
+ 
+
+   #equation2.set('Value in Hex')
+
+ 
+
+   button_1 = Button(frame , text = '1', command=pressed_button_1)
+
+   button_1.grid(column = 1 ,row =1)
+
+ 
+
+ 
+
+  button_2 = Button(frame , text = '4',command=pressed_button_4 )
+
+   button_2.grid(column = 1 ,row = 2)
+
+  
+
+   button_3 = Button(frame , text = '7',command=pressed_button_7 )
+
+   button_3.grid(column = 1 ,row =3)
+
+  
+
+   button_4 = Button(frame , text = '2',command=pressed_button_2 )
+
+   button_4.grid(column = 2,row =1 )
+
+  
+
+   button_5 = Button(frame , text = '5',command=pressed_button_5 )
+
+   button_5.grid(column = 2,row =2 )
+
+  
+
+   button_6 = Button(frame , text = '8',command=pressed_button_8 )
+
+   button_6.grid(column = 2,row =3 )
+
+  
+
+   button_7 = Button(frame , text = '3',command=pressed_button_3 )
+
+   button_7.grid(column = 3,row=1 )
+
+  
+
+   button_8 = Button(frame , text = '6',command=pressed_button_6 )
+
+   button_8.grid(column = 3 ,row=2)
+
+  
+
+   button_9 = Button(frame , text = '9',command=pressed_button_9 )
+
+   button_9.grid(column = 3,row =3  )
+
+  
+
+   button_0 = Button(frame , text = '0',command=pressed_button_0)
+
+   button_0.grid(column = 2 )
+
+ 
+
+   button_add = Button(frame , text = '+' ,command=pressed_button_add )
+
+   button_add.grid(column=1,row=4 )
+
+ 
+
+   button_sub = Button(frame , text = '-',command=pressed_button_sub )
+
+   button_sub.grid(column = 3, row = 4 )
+
+ 
+
+   button_mul = Button(frame , text = '*',command=pressed_button_mul )
+
+   button_mul.grid(column = 4, row =4  )
+
+  
+
+   button_div = Button(frame , text = '/',command=pressed_button_div )
+
+   button_div.grid(column = 3, row =5  )
+
+   root.mainloop()
+
+  
+
+main()
